81fc86032a8e617b7cbc09a55c45826797b48a7d
Juan Reyes <juanfe_518@hotmail.com>
2019-07-09 16:50:55 -0500

    add HEAD, PATCH, PUT and DELETE Test

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -27,5 +27,57 @@ describe('First Api Tests', () => {
           });
       });
 
-     
+      it('Consume HEAD Service', () => {
+        return agent.head('https://httpbin.org/get')
+          .then((response) => {
+              expect(response.status).to.equal(statusCode.OK);
+              expect(response.body).to.be.empty;
+          })
+      })
+
+      it('Consume PATCH Service', () => {
+        const query = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+          };
+
+        return agent.patch('https://httpbin.org/patch')
+          .query(query)
+          .then((response) => {
+              expect(response.status).to.equal(statusCode.OK);
+              expect(response.body.args).to.eql(query);
+          })
+      })
+
+      it('Consume PUT Service', () => {
+        const query = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+          };
+
+        
+        return agent.put('https://httpbin.org/put')
+          .query(query)
+          .then((response) => {
+              expect(response.status).to.equal(statusCode.OK);
+              expect(response.body.args).to.eql(query);
+          })
+      })
+
+      it('Consume DELETE Service', () => {
+        const query = {
+            name: 'John',
+            age: '31',
+            city: 'New York'
+          };
+
+        return agent.del('https://httpbin.org/delete')
+          .query(query)
+          .then((response) => {
+              expect(response.status).to.equal(statusCode.OK);
+              expect(response.body.args).to.eql(query);
+          })
+      })
 });
