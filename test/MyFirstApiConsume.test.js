412732e2a1d924c372feed0b7588a92504bf985d
Juan Felipe Reyes Garcia <juanfe_518@hotmail.com>
2019-07-12 13:20:09 -0500

    mod async/await (#8)

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -4,85 +4,76 @@ const statusCode = require('http-status-codes');
 const urlBase = 'https://httpbin.org';
 describe(`Given the ${urlBase} is Up`, () => {
   describe('when the /ip resource is called', () => {
-    it('then it should respond OK and have property origin', () => agent.get(`${urlBase}/ip`).then((response) => {
+    it('then it should respond OK and have property origin', async () => {
+      const response = await agent.get(`${urlBase}/ip`);
       expect(response.status).toBe(statusCode.OK);
       expect(response.body).toHaveProperty('origin');
-    }));
+    });
   });
 
   describe('when the /ip resource is called with QueryParams', () => {
-    it('then it should respond OK and have property origin', () => {
+    it('then it should respond OK and have property origin', async () => {
       const query = {
         name: 'John',
         age: '31',
         city: 'New York'
       };
-
-      return agent.get(`${urlBase}/get`)
-        .query(query)
-        .then((response) => {
-          expect(response.status).toBe(statusCode.OK);
-          expect(response.body.args).toEqual(query);
-        });
+      const response = await agent.get(`${urlBase}/get`).query(query);
+      expect(response.status).toBe(statusCode.OK);
+      expect(response.body.args).toEqual(query);
     });
   });
 
   describe('when the /get resource is called with the HEAD method', () => {
-    it('then it should respond OK and the body must be empty', () => agent.head(`${urlBase}/get`)
-      .then((response) => {
-        const emptyBody = {};
-        expect(response.status).toBe(statusCode.OK);
-        expect(response.body).toEqual(emptyBody);
-      }));
+    it('then it should respond OK and the body must be empty', async () => {
+      const response = await agent.head(`${urlBase}/get`);
+      const emptyBody = {};
+      expect(response.status).toBe(statusCode.OK);
+      expect(response.body).toEqual(emptyBody);
+    });
   });
 
   describe('when the /patch resource is called with QueryParams', () => {
-    it('then it should respond OK and the body must contain the QueryParams in the request', () => {
+    it('then it should respond OK and the body must contain the QueryParams in the request', async () => {
       const query = {
         name: 'John',
         age: '31',
         city: 'New York'
       };
 
-      return agent.patch(`${urlBase}/patch`)
-        .query(query)
-        .then((response) => {
-          expect(response.status).toBe(statusCode.OK);
-          expect(response.body.args).toEqual(query);
-        });
+      const response = await agent.patch(`${urlBase}/patch`)
+        .query(query);
+      expect(response.status).toBe(statusCode.OK);
+      expect(response.body.args).toEqual(query);
     });
   });
 
   describe('when the /put resource is called with QueryParams', () => {
-    it('then it should respond OK and the body must contain the QueryParams in the request', () => {
+    it('then it should respond OK and the body must contain the QueryParams in the request', async () => {
       const query = {
         name: 'John',
         age: '31',
         city: 'New York'
       };
-      return agent.put(`${urlBase}/put`)
-        .query(query)
-        .then((response) => {
-          expect(response.status).toBe(statusCode.OK);
-          expect(response.body.args).toEqual(query);
-        });
+      const response = await agent.put(`${urlBase}/put`)
+        .query(query);
+      expect(response.status).toBe(statusCode.OK);
+      expect(response.body.args).toEqual(query);
     });
   });
 
   describe('when the /delete resource is called with QueryParams', () => {
-    it('then it should respond OK and the body must contain the QueryParams in the request', () => {
+    it('then it should respond OK and the body must contain the QueryParams in the request', async () => {
       const query = {
         name: 'John',
         age: '31',
         city: 'New York'
       };
 
-      return agent.del(`${urlBase}/delete`)
-        .query(query)
-        .then((response) => {
-          expect(response.status).toBe(statusCode.OK);
-          expect(response.body.args).toEqual(query);
-        });
+      const response = await agent.del(`${urlBase}/delete`)
+        .query(query);
+      expect(response.status).toBe(statusCode.OK);
+      expect(response.body.args).toEqual(query);
     });
   });
 });
