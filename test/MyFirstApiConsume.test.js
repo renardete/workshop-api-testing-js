628edde38533abdfff412198c7e1b72099923d09
Juan Reyes <juanfe_518@hotmail.com>
2019-07-12 11:17:52 -0500

    mod to BDD specification

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -2,78 +2,87 @@ const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
 
 const urlBase = 'https://httpbin.org';
-
-describe('First Api Tests', () => {
-  it('Consume GET Service', () => agent.get(`${urlBase}/ip`).then((response) => {
-    expect(response.status).toBe(statusCode.OK);
-    expect(response.body).toHaveProperty('origin');
-  }));
-
-  it('Consume GET Service with query parameters', () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
-    };
-
-    return agent.get(`${urlBase}/get`)
-      .query(query)
-      .then((response) => {
-        expect(response.status).toBe(statusCode.OK);
-        expect(response.body.args).toEqual(query);
-      });
-  });
-
-  it('Consume HEAD Service', () => agent.head(`${urlBase}/get`)
-    .then((response) => {
-      const emptyBody = {};
+describe(`Given the ${urlBase} is Up`, () => {
+  describe('When the /ip resource is called', () => {
+    it('Then it should respond OK and have property origin', () => agent.get(`${urlBase}/ip`).then((response) => {
       expect(response.status).toBe(statusCode.OK);
-      expect(response.body).toEqual(emptyBody);
+      expect(response.body).toHaveProperty('origin');
     }));
-
-  it('Consume PATCH Service', () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
-    };
-
-    return agent.patch(`${urlBase}/patch`)
-      .query(query)
-      .then((response) => {
-        expect(response.status).toBe(statusCode.OK);
-        expect(response.body.args).toEqual(query);
-      });
   });
 
-  it('Consume PUT Service', () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
-    };
+  describe('When the /ip resource is called with QueryParams', () => {
+    it('Then it should respond OK and have property origin', () => {
+      const query = {
+        name: 'John',
+        age: '31',
+        city: 'New York'
+      };
 
-
-    return agent.put(`${urlBase}/put`)
-      .query(query)
-      .then((response) => {
-        expect(response.status).toBe(statusCode.OK);
-        expect(response.body.args).toEqual(query);
-      });
+      return agent.get(`${urlBase}/get`)
+        .query(query)
+        .then((response) => {
+          expect(response.status).toBe(statusCode.OK);
+          expect(response.body.args).toEqual(query);
+        });
+    });
   });
 
-  it('Consume DELETE Service', () => {
-    const query = {
-      name: 'John',
-      age: '31',
-      city: 'New York'
-    };
-
-    return agent.del(`${urlBase}/delete`)
-      .query(query)
+  describe('When the /get resource is called with the HEAD method', () => {
+    it('Then it should respond OK and the body must be empty', () => agent.head(`${urlBase}/get`)
       .then((response) => {
+        const emptyBody = {};
         expect(response.status).toBe(statusCode.OK);
-        expect(response.body.args).toEqual(query);
-      });
+        expect(response.body).toEqual(emptyBody);
+      }));
+  });
+
+  describe('When the /patch resource is called with QueryParams', () => {
+    it('Then it should respond OK and the body must contain the QueryParams in the request', () => {
+      const query = {
+        name: 'John',
+        age: '31',
+        city: 'New York'
+      };
+
+      return agent.patch(`${urlBase}/patch`)
+        .query(query)
+        .then((response) => {
+          expect(response.status).toBe(statusCode.OK);
+          expect(response.body.args).toEqual(query);
+        });
+    });
+  });
+
+  describe('When the /put resource is called with QueryParams', () => {
+    it('Then it should respond OK and the body must contain the QueryParams in the request', () => {
+      const query = {
+        name: 'John',
+        age: '31',
+        city: 'New York'
+      };
+      return agent.put(`${urlBase}/put`)
+        .query(query)
+        .then((response) => {
+          expect(response.status).toBe(statusCode.OK);
+          expect(response.body.args).toEqual(query);
+        });
+    });
+  });
+
+  describe('When the /delete resource is called with QueryParams', () => {
+    it('Consume DELETE Service', () => {
+      const query = {
+        name: 'John',
+        age: '31',
+        city: 'New York'
+      };
+
+      return agent.del(`${urlBase}/delete`)
+        .query(query)
+        .then((response) => {
+          expect(response.status).toBe(statusCode.OK);
+          expect(response.body.args).toEqual(query);
+        });
+    });
   });
 });
