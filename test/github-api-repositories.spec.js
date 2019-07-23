0237e985a1895692303082f66984f37639a528ef
Juan Reyes <juanfe_518@hotmail.com>
2019-07-23 17:03:29 -0500

    mod beforeAll() blocks added for http calls

--- test/github-api-repositories.spec.js
+++ test/github-api-repositories.spec.js
@@ -8,10 +8,14 @@ const githubUserName = 'aperdomob';
 
 describe(`Given than ${urlBase} is UP`, () => {
   describe(`when the resource ${urlBase}/users/${githubUserName} is called`, () => {
-    it('then the response body should contain the name, company and location of the user', async () => {
-      const response = await agent
+    let response = {};
+    beforeAll(async () => {
+      response = await agent
         .get(`${urlBase}/users/${githubUserName}`)
         .set('User-Agent', 'agent');
+    });
+
+    it('then the response body should contain the name, company and location of the user', async () => {
       expect(response.status).toBe(statusCode.OK);
       expect(response.body.name).toBe('Alejandro Perdomo');
       expect(response.body.company).toBe('PSL');
@@ -20,15 +24,19 @@ describe(`Given than ${urlBase} is UP`, () => {
   });
 
   describe('when the resource of repositories of user is called', () => {
-    it('then response body should contain the name, publication state and description of the repo', async () => {
-      let response = await agent
+    let response = {};
+    let jasmineAwesomeRepository = {};
+    beforeAll(async () => {
+      response = await agent
         .get(`${urlBase}/users/${githubUserName}`)
         .set('User-Agent', 'agent');
       response = await agent
         .get(response.body.repos_url)
         .set('User-Agent', 'agent');
-      const jasmineAwesomeRepository = await response.body
+      jasmineAwesomeRepository = await response.body
         .find(respository => respository.name === 'jasmine-awesome-report');
+    });
+    it('then response body should contain the name, publication state and description of the repo', async () => {
       expect(response.status).toBe(statusCode.OK);
       expect(jasmineAwesomeRepository.name).toBe('jasmine-awesome-report');
       expect(jasmineAwesomeRepository.private).toBe(false);
@@ -37,41 +45,56 @@ describe(`Given than ${urlBase} is UP`, () => {
   });
 
   describe('when the resource of zip download of repositories is called', () => {
-    it('Zip download', async () => {
-      let response = await agent
+    let response = {};
+    let jasmineAwesomeRepository = {};
+    let zipUrl = '';
+    let exists = false;
+    let zipFilePath = '';
+
+    beforeAll(async () => {
+      response = await agent
         .get(`${urlBase}/users/${githubUserName}`)
         .set('User-Agent', 'agent');
       response = await agent
         .get(response.body.repos_url)
         .set('User-Agent', 'agent');
-      const jasmineAwesomeRepository = await response.body
+      jasmineAwesomeRepository = await response.body
         .find(respository => respository.name === 'jasmine-awesome-report');
-      let zipUrl = await jasmineAwesomeRepository.archive_url.replace('{archive_format}', 'zipball');
-      const zipFilePath = './file-downloads/jasmine.zip';
+      zipUrl = await jasmineAwesomeRepository.archive_url.replace('{archive_format}', 'zipball');
+      zipFilePath = './file-downloads/jasmine.zip';
       zipUrl = await zipUrl.replace('{/ref}', '/master');
       agent
         .get(zipUrl)
         .set('User-Agent', 'agent')
         .pipe(fs.createWriteStream(zipFilePath));
-      const exists = fs.existsSync(zipFilePath);
+      exists = fs.existsSync(zipFilePath);
+    });
+    it('Zip download', async () => {
       expect(exists).toBe(true);
     }, 15000);
   });
 
   describe('when contents resouce of a repositorie is called', () => {
-    it('then the response body should contain the name, path and sha of the README.md file', async () => {
-      let response = await agent.get(`${urlBase}/users/${githubUserName}`)
+    let response = {};
+    let jasmineAwesomeRepository = {};
+    let contentUrl = '';
+    let readmeInfo = {};
+
+    beforeAll(async () => {
+      response = await agent.get(`${urlBase}/users/${githubUserName}`)
         .set('User-Agent', 'agent');
       response = await agent.get(response.body.repos_url)
         .set('User-Agent', 'agent');
-      const jasmineAwesomeRepository = await response.body
+      jasmineAwesomeRepository = await response.body
         .find(respository => respository.name === 'jasmine-awesome-report');
-      const contentUrl = await jasmineAwesomeRepository.contents_url.replace('/{+path}', '');
+      contentUrl = await jasmineAwesomeRepository.contents_url.replace('/{+path}', '');
       response = await agent
         .get(contentUrl)
         .set('User-Agent', 'agent');
-      const readmeInfo = await response.body.find(content => content.name === 'README.md');
+      readmeInfo = await response.body.find(content => content.name === 'README.md');
+    });
 
+    it('then the response body should contain the name, path and sha of the README.md file', async () => {
       expect(response.status).toBe(statusCode.OK);
       expect(readmeInfo.name).toBe('README.md');
       expect(readmeInfo.path).toBe('README.md');
@@ -80,21 +103,30 @@ describe(`Given than ${urlBase} is UP`, () => {
   });
 
   describe('when README.md resource is called', () => {
-    it('then the download must be succesfull', async () => {
-      let response = await agent.get(`${urlBase}/users/${githubUserName}`)
+    let response = {};
+    let jasmineAwesomeRepository = {};
+    let contentUrl = '';
+    let exists = false;
+    let readmeInfo = {};
+
+    beforeAll(async () => {
+      response = await agent.get(`${urlBase}/users/${githubUserName}`)
         .set('User-Agent', 'agent');
       response = await agent.get(response.body.repos_url)
         .set('User-Agent', 'agent');
-      const jasmineAwesomeRepository = await response.body
+      jasmineAwesomeRepository = await response.body
         .find(respository => respository.name === 'jasmine-awesome-report');
-      const contentUrl = await jasmineAwesomeRepository.contents_url.replace('/{+path}', '');
+      contentUrl = await jasmineAwesomeRepository.contents_url.replace('/{+path}', '');
       response = await agent
         .get(contentUrl)
         .set('User-Agent', 'agent');
-      const readmeInfo = await response.body.find(content => content.name === 'README.md');
+      readmeInfo = await response.body.find(content => content.name === 'README.md');
       await download(readmeInfo.download_url, 'file-downloads');
 
-      const exists = fs.existsSync('./file-downloads/README.md');
+      exists = fs.existsSync('./file-downloads/README.md');
+    });
+
+    it('then the download must be succesfull', async () => {
       expect(exists).toBe(true);
     });
   });
