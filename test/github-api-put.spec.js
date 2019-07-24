404c4d73aaac20fc18f7f35b0128f3bec31efdd8
Juan Reyes <juanfe_518@hotmail.com>
2019-07-24 08:43:20 -0500

    add github-api-put.spec.js

---
+++ test/github-api-put.spec.js
@@ -0,0 +1,67 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+
+const urlBase = 'https://api.github.com';
+
+describe(`Given ${urlBase} domain is up`, () => {
+  describe('when https://api.github.com/user/following/aperdomob resource is calles', () => {
+    const followUrl = `${urlBase}/user/following/aperdomob`;
+    const emptyBody = {};
+    let response = {};
+
+    beforeAll(async () => {
+      response = await agent
+        .put(followUrl)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .set('User-Agent', 'agent');
+    });
+
+    it('then the response should contain status-code = 204 and the body must be empty', async () => {
+      response = await agent
+        .put(followUrl)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .set('User-Agent', 'agent');
+      expect(response.statusCode).toBe(statusCode.NO_CONTENT);
+      expect(response.body).toEqual(emptyBody);
+    });
+  });
+
+  describe('when https://api.github.com/user/following resource is called', () => {
+    const followUrl = `${urlBase}/user/following`;
+    const followedName = 'aperdomob';
+    const followedId = 17033942;
+    let response = {};
+    let followedUser = {};
+
+    beforeAll(async () => {
+      response = await agent
+        .get(followUrl)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .set('User-Agent', 'agent');
+      followedUser = response.body.find(user => user.login === followedName);
+    });
+
+    it(`then the response should contain status-code OK (200) and the body must contain login: ${followedName} and id: ${followedId}`, async () => {
+      expect(response.statusCode).toBe(statusCode.OK);
+      expect(followedUser.login).toBe(followedName);
+      expect(followedUser.id).toBe(followedId);
+    });
+  });
+
+  describe('when https://api.github.com/user/following/aperdomob resource is calles', () => {
+    const followUrl = `${urlBase}/user/following/aperdomob`;
+    it('then the response should contain status-code = 204 and the body must be empty', async () => {
+      const response1 = await agent
+        .put(followUrl)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .set('User-Agent', 'agent');
+
+      const response2 = await agent
+        .put(followUrl)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .set('User-Agent', 'agent');
+      expect(response1.statusCode).toBe(response2.statusCode);
+      expect(response1.body).toEqual(response2.body);
+    });
+  });
+});
