afbdf9d0f357469bb526904f7eaef69c877dc68e
Juan Reyes <juanfe_518@hotmail.com>
2019-07-30 16:45:55 -0500

    add redirect test

--- utils/http-utils.js
+++ utils/http-utils.js
@@ -11,8 +11,15 @@ module.exports = {
   authHeadSync: async function authHeadSync(url) {
     const response = await agent
       .head(url)
+      .redirects(0)
       .auth('token', process.env.ACCESS_TOKEN)
-      .set('User-Agent', 'agent');
+      .set('User-Agent', 'agent')
+      .ok(res => {
+        if (res.status === 301)
+        
+        return res;
+      });
+    console.log(response.status)
     return response;
   }
 }
