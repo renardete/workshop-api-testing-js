99d571a07a27113e28651a11a50d46eea304ce4b
Juan Reyes <juanfe_518@hotmail.com>
2019-07-30 16:47:08 -0500

    reformat

---
+++ test/github-api-redirect.spec.js
@@ -0,0 +1,20 @@
+// const statusCode = require('http-status-codes');
+const httpUtils = require('../utils/http-utils');
+
+const urlBase = 'https://api.github.com';
+
+describe(`Given ${urlBase} domain is up`, () => {
+  describe('when ', () => {
+    const redirectTestUrl = 'https://github.com/aperdomob/redirect-test';
+    let response = {};
+
+    beforeAll(async () => {
+      response = await httpUtils.authHeadSync(redirectTestUrl);
+    });
+
+    it('then', () => {
+      expect(response.status).toBe(301);
+      expect(response.headers.location).toBe()
+    });
+  });
+});
---
+++ utils/http-utils.js
@@ -0,0 +1,26 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+
+module.exports = {
+  authGetSync: async function authGetSync(url) {
+    const response = await agent
+      .get(url)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent');
+    return response;
+  },
+  authHeadSync: async function authHeadSync(url) {
+    const response = await agent
+      .head(url)
+      .redirects(0)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent')
+      .ok(res => {
+        if (res.status === 301)
+        
+        return res;
+      });
+    return response;
+  }
+}
+
+
