a902181fcaf3b1ae43199995dedf072cff9a2ebb
Juan Reyes <juanfe_518@hotmail.com>
2019-07-31 10:29:11 -0500

    add user-resource test

--- utils/http-utils.js
+++ utils/http-utils.js
@@ -1,4 +1,5 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
+const responseTime = require('superagent-response-time');
 
 module.exports = {
   authGetSync: async function authGetSync(url) {
@@ -15,10 +16,21 @@ module.exports = {
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent')
       .ok(res => {
-        if (res.status === 301)
-          return res;
+        if (res.status === 301)        
+        return res;
       });
     return response;
+  },
+  authGetResponseTimeSync: async function authResponseTimeSync(url) {
+    let ResponseTime = Number.MAX_SAFE_INTEGER;
+    await agent
+      .get(url)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent')
+      .use(responseTime( async (req, time) => {
+        ResponseTime = time;
+      }));
+    return ResponseTime;
   }
 }
 
