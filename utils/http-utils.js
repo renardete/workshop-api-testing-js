06ed3c23d7db8844941466aafcec221d665bdbab
Juan Felipe Reyes Garcia <juanfe_518@hotmail.com>
2019-07-31 09:58:06 -0500

    Head services (#79)
    * reformat
    
    * rebase github-api-redirect.spec.js
    
    * add redirect test
    
    * add get redirect resource
    
    * mod then detail for GET test
    
    * del console.log() and whiteSpace
    
    * global var for test suite
    
    * lint

--- utils/http-utils.js
+++ utils/http-utils.js
@@ -7,6 +7,18 @@ module.exports = {
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent');
     return response;
+  },
+  authHeadSync: async function authHeadSync(url) {
+    const response = await agent
+      .head(url)
+      .redirects(0)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent')
+      .ok(res => {
+        if (res.status === 301)
+          return res;
+      });
+    return response;
   }
 }
 
