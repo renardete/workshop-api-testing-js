48c5ff72f42f5f8c03b6956988613a62a059126d
Juan Reyes <juanfe_518@hotmail.com>
2019-08-02 17:01:35 -0500

    scheme validation for array - must validate http req1uest

--- schema/list-public-events-schema.js
+++ schema/list-public-events-schema.js
@@ -1,71 +1,15 @@
 const listPublicEventsSchema = {
-    properties: [
-        {
+    properties: {
             id: { type: 'string' },
             type: { type: 'string' },
             actor: { type: 'object' },
-            repo: {
-                id: { type: 'number' },
-                name: { type: 'string' },
-                url: { type: 'string' }
-            },
-            "payload": {
-                "action": { type: 'string' },
-                "number": { type: 'number' },
-                "pull_request": {
-                    "url": { type: 'string' },
-                    "id": { type: 'number' },
-                    "node_id": { type: 'string' },
-                    "html_url": { type: 'string' },
-                    "diff_url": { type: 'string' },
-                    "patch_url": { type: 'string' },
-                    "issue_url": { type: 'string' },
-                    "number": { type: 'number' },
-                    "state": { type: 'string' },
-                    "locked": { type: 'boolean' },
-                    "title": { type: 'string' },
-                    "user": { type: 'object' },
-                    "body": { type: 'string' },
-                    "created_at": { type: 'string' },
-                    "updated_at": { type: 'string' },
-                    "closed_at": { type: 'string' },
-                    "merged_at": { type: 'string' },
-                    "merge_commit_sha": { type: 'string' },
-                    "assignee": null,
-                    "assignees": { type: 'array' },
-                    "requested_reviewers": { type: 'array' },
-                    "requested_teams": { type: 'array' },
-                    "labels": { type: 'array' },
-                    "milestone": null,
-                    "commits_url": { type: 'string' },
-                    "review_comments_url": { type: 'string' },
-                    "review_comment_url": { type: 'string' },
-                    "comments_url": { type: 'string' },
-                    "statuses_url": { type: 'string' },
-                    "head": { type: 'object' },
-                    "base": { type: 'object' },
-                    "_links": { type: 'object' },
-                    "comments": { type: 'number' },
-                    "review_comments": { type: 'number' },
-                    "maintainer_can_modify": { type: 'boolean' },
-                    "commits": { type: 'number' },
-                    "additions": { type: 'number' },
-                    "deletions": { type: 'number' },
-                    "changed_files": { type: 'number' }
-                }
-            },
-            "public": { type: 'boolean' },
-            "created_at": { type: 'string' },
-            "org": {
-                "id": { type: 'number' },
-                "login": { type: 'string' },
-                "gravatar_id": { type: 'string' },
-                "url": { type: 'string' },
-                "avatar_url": { type: 'string' }
-            }
-        }
-    ],
-    required: ['id']
+            repo:{ type: 'object' },
+            payload: { type: 'object' },
+            public: { type: 'boolean' },
+            created_at: { type: 'string' },
+            org: { type: 'object' }
+        },
+    required: ['id', 'type', 'actor', 'repo', 'payload', 'public', 'created_at', 'org']
 };
 
 
--- test/github-api-contract.spec.js
+++ test/github-api-contract.spec.js
@@ -1,54 +1,38 @@
+/* global provider */
+
+const { matchers } = require('jest-json-schema');
+
 const agent = require('superagent');
-const listPublicEventsSchema = require('../schema/list-public-events-schema');
-const { Pact } = require("@pact-foundation/pact")
-const { Matchers } = require("@pact-foundation/pact")
+const { listPublicEventsSchema } = require('../schema/list-public-events-schema');
+
+const { listPublicEventsExp } = require('../schema/list-public-events-exp');
+
+
+expect.extend(matchers);
+
+
 const urlBase = 'https://api.github.com';
+const urlEvents = `${urlBase}/events`;
 
 describe('Given event Github API resources', () => {
   describe('When wanna verify the List public events', () => {
     let EXPECTED_BODY;
+    const schema = listPublicEventsSchema;
 
-    beforeAll(async () => {
+    beforeEach(async () => {
       EXPECTED_BODY = await agent
-        .get(`${urlBase}/events`)
+        .get(urlEvents)
         .set('User-Agent', 'agent')
         .auth('token', process.env.ACCESS_TOKEN);
-
-      const interaction = {
-        state: "i have a list of projects",
-        uponReceiving: "a request for projects",
-        withRequest: {
-          method: "GET",
-          path: "/dogs",
-          headers: {
-            Accept: "application/json",
-          },
-        },
-        willRespondWith: {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: Matchers.like(EXPECTED_BODY.body),
-        },
-      }
-      return provider.addInteraction(interaction)
+      EXPECTED_BODY.body = listPublicEventsExp;
     });
 
 
-
-    it('then the body should have a schema', done => {
-      return getMeDogs({
-        url,
-        port,
-      })
-        .then(response => {
-          expect(response.headers["content-type"]).toEqual("application/json")
-          expect(response.data).toEqual(EXPECTED_BODY)
-          expect(response.status).toEqual(200)
-          done()
-        })
-        .then(() => provider.verify())
+    it('then the body should have fulfill the pact', async (done) => {
+      for (let i = 0; i < EXPECTED_BODY.body.length; i += 1) {
+        expect(EXPECTED_BODY.body[0]).toMatchSchema(schema);
+      }
+      done();
     });
   });
 });
