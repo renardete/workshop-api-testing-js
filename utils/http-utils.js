6fe635c00e42a37ade3c89985f6c61156ab9606b
Juan Reyes <juanfe_518@hotmail.com>
2019-08-05 13:23:20 -0500

    sove conflcts merge

--- utils/http-utils.js
+++ utils/http-utils.js
@@ -1,11 +1,7 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
-<<<<<<< HEAD
-const responseTime = require('superagent-response-time');
-=======
 const agentResponseTime = require('superagent-response-time');
 const statusCode = require('http-status-codes');
 
->>>>>>> master
 
 module.exports = {
   authGetSync: async (url) => {
@@ -27,29 +23,15 @@ module.exports = {
       });
     return response;
   },
-<<<<<<< HEAD
-  authGetResponseTimeSync: async function authResponseTimeSync(url) {
-    let ResponseTime = Number.MAX_SAFE_INTEGER;
-=======
   authGetResponseTimeSync: async (url) => {
     let responseTime = undefined;
->>>>>>> master
     await agent
       .get(url)
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent')
-<<<<<<< HEAD
-      .use(responseTime( async (req, time) => {
-        ResponseTime = time;
-      }));
-
-    return ResponseTime;
-
-=======
       .use(agentResponseTime((req, time) => {
         responseTime = time;
       }));
     return responseTime;
->>>>>>> master
   }
 }
