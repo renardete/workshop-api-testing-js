fc5cc26d1003fe03ebbcae6531e5fb6aa001b67d
Juan Reyes <juanfe_518@hotmail.com>
2019-08-05 13:20:25 -0500

    mod users.spec

--- test/github-api-users.spec.js
+++ test/github-api-users.spec.js
@@ -1,41 +1,44 @@
-// const statusCode = require('http-status-codes');
 const httpUtils = require('../utils/http-utils');
 
 const urlBase = 'https://api.github.com';
+const urlTenUsers = `${urlBase}/users?per_page=10`;
+const urlFiftyUsers = `${urlBase}/users?per_page=50`;
 
 describe(`Given ${urlBase} domain is up`, () => {
   describe(`when resource ${urlBase}/users is called `, () => {
     let time = null;
     const usersUrl = `${urlBase}/users`;
     const expectedTime = 5000;
+
     beforeAll(async () => {
       time = await httpUtils.authGetResponseTimeSync(usersUrl);
     });
-
     it(`then the response time should be inferior to ${expectedTime} miliseconds`, () => {
       expect(time).toBeLessThan(expectedTime);
     });
   });
 
-  describe(`when resource ${urlBase}/users?per_page=10 is called `, () => {
+  describe(`when resource ${urlTenUsers} is called `, () => {
     let response = {};
+
     beforeAll(async () => {
-      response = await httpUtils.authGetSync(`${urlBase}/users?per_page=10`);
+      response = await httpUtils.authGetSync(urlTenUsers);
     });
 
-    it('then the body of the response should contain 10 users', () => {
+    it('then the body of the response should contain a list of 10 users', () => {
       const numberUsers = response.body.length;
       expect(numberUsers).toBe(10);
     });
   });
 
-  describe(`when resource ${urlBase}/users?per_page=50 is called `, () => {
+  describe(`when resource ${urlFiftyUsers} is called `, () => {
     let response = {};
+
     beforeAll(async () => {
-      response = await httpUtils.authGetSync(`${urlBase}/users?per_page=50`);
+      response = await httpUtils.authGetSync(urlFiftyUsers);
     });
 
-    it('then the body of the response should contain 50 users', () => {
+    it('then the body of the response should contain a list of 50 users', () => {
       const numberUsers = response.body.length;
       expect(numberUsers).toBe(50);
     });
